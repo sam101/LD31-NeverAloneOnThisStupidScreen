7f7683299309638fcd67f54aa82c5c512097a2b2
Samuel Lepetit <lepetit.samuel@gmail.com>
2014-12-06 00:29:56 -0500

    Working renderer with resize

--- static/js/game.js
+++ static/js/game.js
@@ -5,14 +5,28 @@ function Game() {
     this.stage = new PIXI.Stage(0x0);
     this.stage.interactive = true;
 
-    this.renderer = PIXI.autoDetectRenderer(common.RENDER_WIDTH, common.RENDER_HEIGHT);
+    this.renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.view);
 
     this.container = new PIXI.DisplayObjectContainer();
+    this.sizeContainer();
+
     this.stage.addChild(this.container);
 
     requestAnimationFrame(render);
 }
+
+Game.prototype.sizeContainer = function() {
+    var currentWidthScaleRatio = window.innerWidth / common.RENDER_WIDTH;
+    var currentHeightScaleRatio = window.innerHeight / common.RENDER_HEIGHT;
+
+    console.log(currentWidthScaleRatio + "," + currentHeightScaleRatio);
+
+    this.renderer.resize(window.innerWidth, window.innerHeight);
+
+    this.container.scale.x = currentWidthScaleRatio;
+    this.container.scale.y = currentHeightScaleRatio;
+};
 
 Game.prototype.startGame = function(initialData) {
     console.log("Let's start the game !");
@@ -23,6 +37,10 @@ Game.prototype.startGame = function(initialData) {
 Game.prototype.draw = function() {
     this.renderer.render(this.stage);
 
+};
+
+window.onresize = function(event) {
+    game.sizeContainer();
 };
 
 function render() {
