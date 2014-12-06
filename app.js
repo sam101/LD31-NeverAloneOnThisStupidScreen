e603956c06a7f9d01cd9a0bea3a010c06e95a1ae
Samuel Lepetit <lepetit.samuel@gmail.com>
2014-12-05 21:37:09 -0500

    Basic stuff for skelton

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ var argv = require('optimist').argv;
 var express =require('express');
 
 var app = express();
+app.set('view engine', 'jade');
+
 var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 
+var env = argv.env || 'production';
+
+app.get('/', function(req, res) {
+    res.render('index', {env: env});
+});
+
+
+app.use('*', function(req, res) {
+    res.send('404 not found', 404);
+});
+
 http.listen(argv.port || 8080);
+
