18c4dde05cf82d8220f2364ecf497c6af5eca667
Samuel Lepetit <lepetit.samuel@gmail.com>
2014-12-07 18:58:00 -0500

    Life bar now show the number of HPs

--- static/js/lifebar.js
+++ static/js/lifebar.js
@@ -14,6 +14,10 @@ function LifeBar(player) {
     this.hp = player.data.hp;
     this.hpMax = player.data.hpMax;
 
+    this.hpText = new PIXI.Text("HP " + this.hp + "/" + this.hpMax, {font:"14px pressstart", fill:"white", dropShadow:true});
+    this.hpText.position.y++;
+    this.addChild(this.hpText);
+
     this.update(player.data.hp, player.data.hpMax);
 
 }
@@ -24,6 +28,7 @@ LifeBar.prototype.update = function(hp, hpMax) {
     if (this.hp == hp && this.hpMax == hpMax) {
         return;
     }
+    this.hpText.setText("HP " + hp + "/" + hpMax);
     common.lifeBar.frame = new PIXI.Rectangle(0,0, hp / hpMax * common.lifeBar.width, common.lifeBar.height);
     this.hp = hp;
     this.hpMax = hpMax;
