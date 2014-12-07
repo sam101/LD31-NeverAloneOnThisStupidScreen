d43b3f077ea6fa65401b0ef763b42c7574549529
Samuel Lepetit <lepetit.samuel@gmail.com>
2014-12-07 17:33:42 -0500

    More monsters/players sprites

--- static/js/common.js
+++ static/js/common.js
@@ -25,13 +25,6 @@ common.emptyBar = PIXI.Texture.fromImage("res/emptybar.png");
 common.lifeBar = PIXI.Texture.fromImage("res/lifebar.png");
 common.expBar = PIXI.Texture.fromImage("res/expbar.png");
 
-common.assetsToLoad = [
-    "res/emptybar.png",
-    "res/laser.png",
-    "res/monsterLaser.png",
-    "res/"
-];
-
 common.monsterSprites = {
     0: {
         texture: PIXI.Texture.fromImage("res/monster0.png")
